725c314a3cb00e0c3b5ec7c6ae632b2b2bea3dcd
Viamatica Uno <viamatica1@nirsa.com>
2023-06-08 15:17:06 -0500

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,15 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'prueba_angular';
+  username:string = "";
+  password:string = "";
+
+  onSubmit() {
+    if (this.username === 'admin' && this.password === 'admin') {
+      // Aquí puedes realizar acciones adicionales después de iniciar sesión exitosamente
+      console.log('Inicio de sesión exitoso');
+    } else {
+      console.log('Inicio de sesión fallido');
+    }
+  }
 }
